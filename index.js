f328d400ac176ac830b4df7ea90bf61e1baf4dc7
giakhanh780f <john1909d@gmail.com>
2020-02-26 21:52:39 -0600

    Added additional queries and CORS support
    Additional queries include getting the sensor ID list and the latest
    combined PM data

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(
         extended: true
     })
 )
+// CORS configuration
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (request, response) => {
     response.json({
