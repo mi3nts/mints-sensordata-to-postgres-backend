0f447326a17581930ece4c9c7c53e2ead26d4d04
giakhanh780f <john1909d@gmail.com>
2020-02-27 23:14:48 -0600

    Added longitude and latitude to latest data queries

--- queries.js
+++ queries.js
@@ -33,10 +33,13 @@ const getSensorData = (request, response) => {
 const getLatestSensorData = (request, response) => {
     const getQuery = "SELECT data_pm1.timestamp, "
             + "data_pm1.value as pm1, "
+            + "data_pm1.sensor_id, "
             + "data_pm2_5.value as pm2_5, "
             + "data_pm10.value as pm10, "
             + "data_pm1.temperature, "
-            + "data_pm1.humidity "
+            + "data_pm1.humidity, "
+            + "data_pm1.longitude, "
+            + "data_pm1.latitude "
             + "FROM data_pm1 "
             + "INNER JOIN data_pm2_5 ON data_pm2_5.timestamp = data_pm1.timestamp "
             + "INNER JOIN data_pm10 ON data_pm10.timestamp = data_pm1.timestamp "
@@ -77,7 +80,9 @@ const getLatestSensorDataForID = (request, response) => {
             "data_pm2_5.value as pm2_5, " +
             "data_pm10.value as pm10, " +
             "data_pm1.temperature, " +
-            "data_pm1.humidity " +
+            "data_pm1.humidity, " +
+            "data_pm1.longitude, " +
+            "data_pm1.latitude " +
         "FROM data_pm1 " +
         "INNER JOIN data_pm2_5 ON data_pm2_5.timestamp = data_pm1.timestamp AND data_pm2_5.sensor_id = data_pm1.sensor_id " +
         "INNER JOIN data_pm10 ON data_pm10.timestamp = data_pm1.timestamp AND data_pm10.sensor_id = data_pm1.sensor_id " +
